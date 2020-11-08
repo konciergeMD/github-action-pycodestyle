7060a27cf6e6e8379be03ecbe98629f1cd5527a3
Kamil Samaj <kamil.samaj@accolade.com>
2020-11-08 10:33:32 +0100

    make entrypoint sh compatible

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
+if [ -z "$GITHUB_TOKEN" ]; then
 	echo "The GITHUB_TOKEN is required."
 	exit 1
 fi
@@ -16,11 +16,11 @@ SUCCESS=$?
 echo "$OUTPUT"
 set -e
 
-if [[ -z "$PRECOMMAND_MESSAGE" ]]; then
+if [ -z "$PRECOMMAND_MESSAGE" ]; then
     echo "No precommand message"
 else
     echo "There is a precommand message"
-    OUTPUT=$PRECOMMAND_MESSAGE$'\n\n'$OUTPUT
+    OUTPUT="${PRECOMMAND_MESSAGE}\n\n${OUTPUT}"
 fi
 
 # If there were errors as part of linting, post a comment. Else, do nothing.
